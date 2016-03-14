c8b7a887ca50ac9207042887d3cbaf19912b3db2
Kimiko Kano <kskano@gmail.com>
2016-03-14 11:00:53 -0600

    Test full_helper method

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,10 +8,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-  end
-
-  test "signup page title" do
     get signup_path
-    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
+    assert_select "title", full_title("Sign up")
   end
 end
