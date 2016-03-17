3bd73be251e3aafa242b0c9953e90e51cbfa6a4c
Kimiko Kano <kskano@gmail.com>
2016-03-17 17:04:14 -0600

    Add form partial for users

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @admin      = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -10,5 +16,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     get signup_path
     assert_select "title", full_title("Sign up")
+    log_in_as(@other_user)
   end
 end
